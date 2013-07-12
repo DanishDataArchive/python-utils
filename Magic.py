e69e68d56f2e74acac1a5b58511db539a225c786
Martin Jensby <marj@dda.dk>
2013-07-12 13:46:30 +0200

    Add a small wrapper for libmagic

---
+++ Magic.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import magic, getopt, sys
+
+if __name__ == "__main__":
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "b:f:m", ["bytes=", "file=", "mime"])
+    except getopt.GetoptError as err:
+        print str(err)
+
+    bytes = 10
+    file = ""
+    mime = False
+
+    for o, a in opts:
+        if o in ("-f", "--file"):
+            file = a
+        elif o in ("-m", "--mime"):
+            mime = True
+        elif o in ("-b", "--bytes"):
+            bytes = int(a)
+
+    if len(file) > 0:
+        print magic.from_buffer(open(file).read(bytes), mime)
+    else:
+        print "Please specify a file"
