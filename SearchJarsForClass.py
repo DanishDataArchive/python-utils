6a3acb620575480c196ded256e915d81b601d8ed
Martin Jensby <marj@dda.dk>
2013-07-23 14:02:56 +0200

    Search a number of jar files for a specific class

---
+++ SearchJarsForClass.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import getopt, sys, os, zipfile
+
+if __name__ == "__main__":
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "c:j:", ["class=", "jars="])
+
+    except getopt.GetoptError as err:
+        print str(err)
+
+    lines = []
+    classes = dict()
+    classToFind = ""
+
+    for o, a in opts:
+        if o in ("-c", "--class"):
+            classToFind = a.strip()
+        elif o in ("-j", "--jars"):
+            jars = open(a)
+            for line in jars.readlines():
+                lines.append(line.rstrip("\n"))
+            jars.close()
+
+    if len(lines) == 0:
+        for line in sys.stdin:
+            lines.append(line.rstrip("\n"))
+
+    if len(lines) == 0:
+        print "Please provide jars to search"
+        sys.exit(-1)
+
+    for file in lines:
+        zFile = zipfile.ZipFile(file)
+
+        for filename in zFile.namelist():
+            if filename.endswith(".class"):
+                key = filename.replace('/', '.').replace(".class", "").strip()
+                classes[key] = file + "!" + filename
+        zFile.close()
+
+    if classes.has_key(classToFind):
+        print classes[classToFind]
