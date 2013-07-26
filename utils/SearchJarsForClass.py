472ce3ee7807fa3c7daf813a6bf0026d1e7a3de0
Martin Jensby <marj@dda.dk>
2013-07-26 14:01:48 +0200

    Add help and verbose options

--- utils/SearchJarsForClass.py
+++ utils/SearchJarsForClass.py
@@ -2,9 +2,15 @@
 
 import getopt, sys, os, zipfile
 
+def printUsage():
+    print os.path.basename(sys.argv[0]) + ", search a number of jar files for class\n"
+    print "-c or --class\t\tclass to find"
+    print "-j or--jars\t\tfile container location of the jars, alternatively you can specify on stdin"
+    print "-v or --verbose\t\tshow which classes are in the jars"
+
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "c:j:", ["class=", "jars="])
+        opts, args = getopt.getopt(sys.argv[1:], "c:j:v", ["class=", "jars=", "verbose"])
 
     except getopt.GetoptError as err:
         print str(err)
@@ -12,6 +18,7 @@ def main():
     lines = []
     classes = dict()
     classToFind = ""
+    verbose = False
 
     for o, a in opts:
         if o in ("-c", "--class"):
@@ -21,6 +28,10 @@ def main():
             for line in jars.readlines():
                 lines.append(line.rstrip("\n"))
             jars.close()
+        elif o in ("-v", "--verbose"):
+            verbose = True
+        elif o in ("-h", "--help"):
+            printUsage()
 
     if len(lines) == 0:
         for line in sys.stdin:
@@ -40,6 +51,9 @@ def main():
                     classes[key].append(file + "!" + filename)
                 else:
                     classes[key] = [file + "!" + filename]
+
+                if verbose:
+                    print "Added " + file + "!" + filename
         zFile.close()
 
     if classes.has_key(classToFind):
