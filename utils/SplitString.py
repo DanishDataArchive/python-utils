9b7e98d68815fe35874aff08a5bc3cca651f457c
Martin Jensby <marj@dda.dk>
2013-08-02 12:00:40 +0200

    Port SplitString.py to OptionParser

--- utils/SplitString.py
+++ utils/SplitString.py
@@ -1,28 +1,31 @@
 #!/usr/bin/env python
 
-import getopt, sys
+import sys, pkg_resources
+
+from optparse import OptionParser
 
 def main():
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "s:d:", ["string=", "delimiter="])
-    except getopt.GetoptError as err:
-        print str(err)
-    
+    parser = OptionParser(version=pkg_resources.require("py-utils-dda")[0].version, epilog="Split a string with a delimiter", description="GPL")
+    parser.add_option("-s", "--string", dest="string", action="store", type="string")
+    parser.add_option("-d", "--delimiter", dest="delimiter", action="store", type="string")
+
+    (opts, args) = parser.parse_args(sys.argv)
+
     delimiter = ":"
     line = ""
-    
-    for o, a in opts:
-        if o in ("-d", "--delimiter"):
-            delimiter = a
-        elif o in ("-s", "--string"):
-            line = a
-    
+
+    if opts.string:
+        line = opts.string
+
+    if opts.delimiter:
+        delimiter = opts.delimiter
+
     if(len(line) == 0):
         print "Please pass a string"
         sys.exit(-1)
-        
+
     splits = line.split(delimiter)
-    
+
     for split in splits:
         print split
 
