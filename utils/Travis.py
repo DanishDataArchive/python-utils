20bf2dcb8d94aa7e18ce8f320fec06a6ecf0f8d1
Martin Jensby <marj@dda.dk>
2013-08-02 14:27:07 +0200

    Add more options

--- utils/Travis.py
+++ utils/Travis.py
@@ -4,32 +4,54 @@ import urllib2, simplejson, sys, pkg_resources
 
 from optparse import OptionParser
 
-def getProjectInfo(parent, projectName):
-    return simplejson.load(urllib2.urlopen("https://travis-ci.org/" + parent + "/" + projectName + ".json"))
+def printBuild(build, printNumber = False):
+    if printNumber:
+        print "\tNumber: " + str(build['number'])
 
-def getLastBuildId(project):
+    if build['config']:
+        if build['config']['jdk']:
+            print "\tJDK: " + build['config']['jdk']
+    print "\tFinished at: " + str(build['finished_at'])
+    print "\tResult: " + str(build['result']) + "\n"
+
+def getProjectInfo(parent, projectName):
+    return simplejson.load(urllib2.urlopen("https://api.travis-ci.org/repos/" + parent + "/" + projectName + ".json"))
+
+def getLastestBuildId(project):
     return project['last_build_id']
 
-def getBuildInfo(parent, projectName, buildNumber):
-    return simplejson.load(urllib2.urlopen("https://travis-ci.org/" + parent + "/" + projectName + "/builds/" + str(buildNumber) + ".json"))
+def getBuilds(parent, projectName):
+    return simplejson.load(urllib2.urlopen("https://api.travis-ci.org/repos/" + parent + "/" + projectName + "/builds.json"))
+
+def printBuildList(buildList):
+    for build in buildList:
+        print "#" + build['number'] + " '" + build['message'] + "' from commit '" + build['commit'][:7] + " (" + build['branch'] + ")' Duration: " + str(build['duration'])
+
+def getBuildFromBuildNumber(parent, projectName, buildNumber):
+    builds = getBuilds(parent, projectName)
+
+    for build in builds:
+        if int(build['number']) == int(buildNumber):
+            return getBuildInfo(parent, projectName, build['id'])
+
+    return None
+
+def getBuildInfo(parent, projectName, buildId):
+    return simplejson.load(urllib2.urlopen("https://api.travis-ci.org/repos/" + parent + "/" + projectName + "/builds/" + str(buildId) + ".json"))
 
 def printBuildDetails(projectName, build):
-    print "Lastest build for " + projectName
-
     print "Build " + str(build['number'])
 
     if len(build['matrix']) > 1:
         for subBuild in build['matrix']:
-            print "\tNumber: " + str(subBuild['number'])
-            print "\tFinished at: " + str(subBuild['finished_at'])
-            print "\tResult: " + str(subBuild['result']) + "\n"
+            printBuild(subBuild, True)
     else:
-        subBuild = build['matrix'][0]
-        print "\tFinished at: " + str(subBuild['finished_at'])
-        print "\tResult: " + str(subBuild['result']) + "\n"
+        printBuild(subBuild)
 
 def main():
     parser = OptionParser(version=pkg_resources.require("py-utils-dda")[0].version, epilog="Fetch info from travis-ci.org", description="GPL")
+    parser.add_option("-a", "--list-builds", dest="listBuilds", action="store_true")
+    parser.add_option("-b", "--build", dest="buildNumber", action="store", type="int")
     parser.add_option("-p", "--project", dest="projectName", action="store", type="string")
     parser.add_option("-l", "--latest-build", dest="latestBuild", action="store_true")
     parser.add_option("-o", "--organization", dest="org", action="store", type="string")
@@ -55,10 +77,21 @@ def main():
         sys.exit(-1)
 
     if opts.latestBuild:
-        lastBuildId = getLastBuildId(project)
+        print "Lastest build for " + projectName
+        lastBuildId = getLastestBuildId(project)
         build = getBuildInfo(parent, projectName, lastBuildId)
 
         printBuildDetails(projectName, build)
+
+    if opts.buildNumber:
+        build = getBuildFromBuildNumber(parent, projectName, opts.buildNumber)
+        if build:
+            printBuildDetails(projectName, build)
+        else:
+            print "Not found"
+
+    if opts.listBuilds:
+        printBuildList(getBuilds(parent, projectName))
 
 if __name__ == '__main__':
     main()
